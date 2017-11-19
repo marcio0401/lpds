77e45c12c4a1a687682b824265230729f9d86801
Marcio Diniz <marcio0401@gmai.com>
2017-11-19 15:20:01 -0300

    Alteração no arquivo sidebar.php

--- resources/views/layouts/painel/sidebar.blade.php
+++ resources/views/layouts/painel/sidebar.blade.php
@@ -5,20 +5,20 @@
 									<li><a><i class="fa fa-table"></i> Configuração<span class="fa fa-chevron-down"></span></a>
 										<ul class="nav child_menu">
 											@can('users-visualizar')
-												<li><a href="{{route('users.index')}}">Usuários</a></li>
+												<li><a href="{{url('users')}}">Usuários</a></li>
 											@endcan
 											@can('roles-visualizar')
-												<li><a href="{{route('roles.index')}}">Perfis</a></li>
+												<li><a href="{{url('roles')}}">Perfis</a></li>
 											@endcan
 											@can('permissions-visualizar')
-												<li><a href="{{route('permissions.index')}}">Permissões</a></li>
+												<li><a href="{{url('permissions')}}">Permissões</a></li>
 											@endcan
 										</ul>
 									</li>
 									<li><a><i class="fa fa-table"></i> Cadastros <span class="fa fa-chevron-down"></span></a>
 										<ul class="nav child_menu">
 											@can('posts-visualizar')
-												<li><a href="{{route('posts.index')}}">Posts</a></li>
+												<li><a href="{{url('posts')}}">Posts</a></li>
 											@endcan
 										</ul>
 									</li>
